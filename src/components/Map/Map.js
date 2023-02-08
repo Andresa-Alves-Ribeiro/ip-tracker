9c9c47a7a39035f7c12a9171449a5f14ee7b7022
Andresa Alves <andresa_15ga@hotmail.com>
2023-02-08 14:20:46 -0300

    alterando importação do leaflet

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import { MyMap } from "./style";
 
-import { Map as MapContent, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
 
 import L from "leaflet";
 
@@ -20,14 +22,14 @@ export default function Map({lat,lon}) {
 
     return (
         <MyMap>
-            <MapContent center={Location} zoom={17}>
+            <MapContainer center={Location} zoom={17} style={{padding: 2}}>
                 <TileLayer
                     attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
                     url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
                 />
 
                 <Marker position={Location} icon={iconLocation}/>
-            </MapContent>
+            </MapContainer>
         </MyMap>
     )
 }
